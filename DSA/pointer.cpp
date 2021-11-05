022b1ee0d28c722326fc0e423e45903dcc443faf
Alim Khan <workingalim@gmail.com>
2021-11-05 21:33:36 +0530

    pointer

--- DSA/pointer.cpp
+++ DSA/pointer.cpp
@@ -1,7 +1,25 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std; 
 
 int main(){
+
+    string s= "Alim";
+    string* ptr = &s;
+    // Pointer stores the address of variable
+    cout<<"ptr : "<< ptr<<endl;
+    cout<<"*ptr : "<<*ptr<<endl;
+    cout<<"&ptr : "<<&ptr<<endl;
+    cout<<"&s : "<<&s<<endl;
+    
+    *ptr = "jafar";
+    cout<<"modified ptr : "<<*ptr<<endl;
+
+    string** doubPtr = &ptr;    
+    cout<<"Making double pointer"<<**doubPtr<<endl;
+
+    **doubPtr = "khan";
+    cout<<"changing the name by doubPtr : "<<**doubPtr<<endl;
+
 
     
 }
